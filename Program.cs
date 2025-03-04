a0e71a0b2fd505f8fe154aba47c8759146caa2ac
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-03-04 11:36:07 +0700

    mengupdate implementasi pada DoorMachine agar bisa berjalan pada main program

--- Program.cs
+++ Program.cs
@@ -11,6 +11,24 @@ namespace tpmodul3_2311104049
             Console.WriteLine("Menampilkan Data Kode Pos : ");
             kodePos.MenampilkanSemuaKodePos();
             Console.WriteLine("");
+
+
+            DoorMachine pintu = new DoorMachine(); // Pintu awal terkunci
+
+            Console.WriteLine(); // Spasi untuk kejelasan output
+
+            pintu.BukaPintu();   // Membuka pintu
+            pintu.StatusPintu(); // Mengecek status pintu
+
+            Console.WriteLine(); // Spasi
+
+            pintu.KunciPintu();  // Mengunci pintu
+            pintu.StatusPintu(); // Mengecek status pintu
+
+            Console.WriteLine(); // Spasi
+
+            pintu.KunciPintu();  // Coba kunci lagi (sudah terkunci)
+            pintu.BukaPintu();   // Coba buka lagi
         }
     }
 }
