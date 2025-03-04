48d5baa4c679b32c12d6731470b15b06db723947
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-03-04 10:45:55 +0700

    menambahkan implementasi agar program bisa berjalan

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace tpmodul3_2311104049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KodePos kodePos = new KodePos();
+            Console.WriteLine("Menampilkan Data Kode Pos : ");
+            kodePos.MenampilkanSemuaKodePos();
+            Console.WriteLine("");
+        }
+    }
+}
