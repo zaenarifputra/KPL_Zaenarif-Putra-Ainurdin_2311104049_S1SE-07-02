dc2210344504549ed33fb71e9aee6a4e3475f175
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-03-04 11:37:16 +0700

    update dependencies lainnya

--- obj/Debug/net8.0/tpmodul3_2311104049.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul3_2311104049.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul3_2311104049")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0183b3775cee108a93814ae1d90d304c9e2d47a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a3ed22d9cfd974f125db79e1e8dcfd31c2f3ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul3_2311104049")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul3_2311104049")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
