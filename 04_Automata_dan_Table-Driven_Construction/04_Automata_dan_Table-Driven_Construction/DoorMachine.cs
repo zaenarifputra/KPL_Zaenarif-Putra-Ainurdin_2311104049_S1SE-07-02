2b3b6401d6a06d163d4789ba6468c171b447e3fa
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-03-05 09:51:56 +0700

    Update Semua Karena Kesalahan

---
+++ 04_Automata_dan_Table-Driven_Construction/04_Automata_dan_Table-Driven_Construction/DoorMachine.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Automata_dan_Table_Driven_Construction
+{
+    public class DoorMachine
+    {
+        public enum State { Terkunci, Terbuka }
+
+        private State currentState;
+
+        public DoorMachine()
+        {
+            currentState = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void BukaPintu()
+        {
+            if (currentState == State.Terkunci)
+            {
+                currentState = State.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci"); // Sesuai permintaan soal
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terbuka");
+            }
+        }
+
+        public void KunciPintu()
+        {
+            if (currentState == State.Terbuka)
+            {
+                currentState = State.Terkunci;
+                Console.WriteLine("Pintu terkunci");
+            }
+            else
+            {
+                Console.WriteLine("Pintu sudah terkunci");
+            }
+        }
+
+        public void StatusPintu()
+        {
+            if (currentState == State.Terkunci)
+                Console.WriteLine("Pintu terkunci");
+            else
+                Console.WriteLine("Pintu tidak terkunci");
+        }
+    }
+}
---
+++ 04_Automata_dan_Table-Driven_Construction/04_Automata_dan_Table-Driven_Construction/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Automata_dan_Table_Driven_Construction
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+            KodePos kodePos = new KodePos();
+            Console.WriteLine("Menampilkan Data Kode Pos : ");
+            kodePos.MenampilkanSemuaKodePos();
+            Console.WriteLine("");
+
+
+            DoorMachine pintu = new DoorMachine(); // Pintu awal terkunci
+
+            Console.WriteLine(); // Spasi untuk kejelasan output
+
+            pintu.BukaPintu();   // Membuka pintu
+            pintu.StatusPintu(); // Mengecek status pintu
+
+            Console.WriteLine(); // Spasi
+
+            pintu.KunciPintu();  // Mengunci pintu
+            pintu.StatusPintu(); // Mengecek status pintu
+
+            Console.WriteLine(); // Spasi
+
+            pintu.KunciPintu();  // Coba kunci lagi (sudah terkunci)
+            pintu.BukaPintu();   // Coba buka lagi
+            Console.ReadLine();
+        }
+    }
+}
