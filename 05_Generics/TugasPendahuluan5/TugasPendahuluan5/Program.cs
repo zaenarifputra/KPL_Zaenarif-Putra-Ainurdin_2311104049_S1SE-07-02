857252db2be2ed40c5ef8348ae34ba31c1b53e53
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-03-07 22:03:19 +0700

    menambahkan class DataGeneric

--- 05_Generics/TugasPendahuluan5/TugasPendahuluan5/Program.cs
+++ 05_Generics/TugasPendahuluan5/TugasPendahuluan5/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+using System;
+
+class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yg tersimpan adalah : {data}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> myData = new DataGeneric<string>("2311104049");
+        myData.PrintData();
+        Console.ReadLine();
+    }
+}
