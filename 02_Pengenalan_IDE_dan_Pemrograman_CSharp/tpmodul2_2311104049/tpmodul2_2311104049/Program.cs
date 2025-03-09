e9a3749164972715b799d2dd6a3468767e98ef5c
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-03-09 15:20:19 +0700

    membuat program Huruf Vokal dan Bilangan Genap

--- 02_Pengenalan_IDE_dan_Pemrograman_CSharp/tpmodul2_2311104049/tpmodul2_2311104049/Program.cs
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/tpmodul2_2311104049/tpmodul2_2311104049/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Menampilkan Huruf Vokal dan Konsonan=== ");
+        Console.WriteLine("Masukkan satu huruf : ");
+        char inputHuruf = Char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        if ("AIUEO".IndexOf(inputHuruf) >= 0)
+        {
+            Console.WriteLine($"Huruf {inputHuruf} merupakan huruf vokal");
+        }
+        else
+        {
+            Console.WriteLine($"Huruf {inputHuruf} merupakan huruf konsonan");
+        }
+        Console.WriteLine();
+
+        //Menampilkan Bilangan Genap dari angka 2
+        Console.WriteLine("=== Menampilakn Daftar Bilangan Genap ===");
+        int[] angkaGenap = new int[5];
+        for (int i = 0; i < 5; i++)
+        {
+            angkaGenap[i] = (i + 1) * 2;
+        }
+
+        for (int i = 0; i < angkaGenap.Length; i++)
+        {
+            Console.WriteLine($"Angka Genap {i + 1} adalah {angkaGenap[i]}");
+        }
+        Console.ReadLine();
+    }
+}
