2c7e7e84b6b2f8c5d54b60a38828091518fa85ba
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-03-10 19:08:22 +0700

    menambahkan class KodeBuah

---
+++ 04_Automata_dan_Table-Driven_Construction/tjmodul4_2311104049/tjmodul4_2311104049/KodeBuah.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace tjmodul4_2311104049
+{
+    class KodeBuah
+    {
+        private static Dictionary<string, string> KodeBuahDict = new Dictionary<string, string>
+        {
+           {"Apel", "A00"}, {"Aprikot", "B00"}, {"Alpukat", "C00"},
+            {"Pisang", "D00"}, {"Paprika", "E00"}, {"Kurma", "K00"},
+            {"Durian", "L00"}, {"Anggur", "M00"}, {"Melon", "N00"},
+            {"Semangka", "O00"}
+        };
+
+        public static string getKodeBuah(string buah)
+        {
+            return KodeBuahDict.ContainsKey(buah) ? KodeBuahDict[buah] : "Kode buah tidak ditemukan";
+        }
+    }
+}
