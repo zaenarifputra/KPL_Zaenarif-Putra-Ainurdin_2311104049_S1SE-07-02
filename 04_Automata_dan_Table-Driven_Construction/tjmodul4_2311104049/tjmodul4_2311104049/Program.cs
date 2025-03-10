5f9002e73ba8f850f6f32bf81935a3e32d5999c8
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-03-10 19:40:14 +0700

    mengupdate agar bisa menjalankan kedua kelas program

--- 04_Automata_dan_Table-Driven_Construction/tjmodul4_2311104049/tjmodul4_2311104049/Program.cs
+++ 04_Automata_dan_Table-Driven_Construction/tjmodul4_2311104049/tjmodul4_2311104049/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tjmodul4_2311104049
+{
+
+    class Program
+{
+    static void Main()
+        {
+            Console.WriteLine("=== Selamat datang di program kode buah ===");
+            Console.WriteLine("Masukkan nama buah: ");
+            string inputBuah = Console.ReadLine();
+            string kode = KodeBuah.getKodeBuah(inputBuah);
+            Console.WriteLine($"Kode buah untuk {inputBuah}: {kode}");
+            Console.ReadLine();
+
+
+            Console.WriteLine("=== Selamat datang di program karakter game ===");
+            Console.Write("Masukkan NIM Anda: ");
+            long nim = long.Parse(Console.ReadLine());
+
+            Karakter karakter = new Karakter(nim);
+
+            // Simulasi perubahan state berdasarkan tombol yang ditekan
+            karakter.TekanTombolS(); // Jongkok
+            karakter.TekanTombolS(); // Tengkurap
+            karakter.TekanTombolW(); // Kembali Jongkok
+            karakter.TekanTombolW(); // Kembali Berdiri
+            karakter.TekanTombolW(); // Terbang
+            karakter.TekanTombolS(); // Kembali Berdiri
+            Console.ReadLine();
+        }
+    }
+}
