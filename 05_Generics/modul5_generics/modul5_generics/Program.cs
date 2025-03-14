9ff4059d098077322d1e450d236697ad1b7e3626
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-03-14 13:31:31 +0700

    menambahkan .gitmodules

---
+++ 05_Generics/modul5_generics/modul5_generics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] arr = { 0, 1, 2, 3, 4 };
+        List<int> list = new List<int>();
+        for (int x = 5; x < 10; x++)
+        {
+            list.Add(x);
+        }
+        ProcessItems<int>(arr);
+        ProcessItems<int>(list);
+    }
+    static void ProcessItems<T>(IList<T> coll)
+    {
+         System.Console.WriteLine("IsReadOnly returns {0} for this collection.",
+         coll.IsReadOnly);
+        foreach (T item in coll)
+        {
+            System.Console.Write(item.ToString() + " ");
+        }
+        System.Console.WriteLine();
+        Console.ReadLine();
+    }
+}
