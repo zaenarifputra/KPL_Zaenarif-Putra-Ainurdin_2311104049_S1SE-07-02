e2cab07d87d6870021f23e55b04a4b8552272875
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-04-15 20:08:19 +0700

    menambahkan class GlossaryItem2311104049

--- 07_Grammar-Based_Input_Processing_Parsing/TJ/TjModul7_Kelompok7/Program.cs
+++ 07_Grammar-Based_Input_Processing_Parsing/TJ/TjModul7_Kelompok7/Program.cs
@@ -5,8 +5,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("=== [2] Menampilkan Parsing Team Members Mahasiswa ===");
-        var teamMembers = new TeamMembers2311104049();
+        Console.WriteLine("=== [3] Menampilkan Parsing Glossary Item Mahasiswa ===");
+        var teamMembers = new GlossaryItem2311104049();
         teamMembers.ReadJSON();
         Console.ReadLine();
     }
