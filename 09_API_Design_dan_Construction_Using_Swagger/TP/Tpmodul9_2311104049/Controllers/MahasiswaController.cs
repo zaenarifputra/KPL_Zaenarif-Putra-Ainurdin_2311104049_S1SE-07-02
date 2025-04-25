034b93b2a039ef907cccb9e2a3fde200701f93bb
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-04-25 15:42:43 +0700

    menambahkan API mahasiswa

---
+++ 09_API_Design_dan_Construction_Using_Swagger/TP/Tpmodul9_2311104049/Controllers/MahasiswaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tpmodul9_2311104049.Models;
+
+namespace Tpmodul9_2311104049.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa>()
+        {
+            new Mahasiswa { Nama = "Zaenarif Putra", NIM = "2311104049" },
+            new Mahasiswa { Nama = "Muhammad Shafiq Rasuna", NIM = "2311104050" },
+            new Mahasiswa { Nama = "Muhammad Ihsan", NIM = "2311104051" },
+
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> Get()
+        {
+            return daftarMahasiswa;
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> Get(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+                return NotFound("Mahasiswa tidak ditemukan");
+            return daftarMahasiswa[index];
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Mahasiswa mhs)
+        {
+            daftarMahasiswa.Add(mhs);
+            return Ok("Mahasiswa berhasil ditambahkan");
+        }
+
+        [HttpDelete("{index}")]
+        public IActionResult Delete(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+                return NotFound("Index tidak valid");
+            daftarMahasiswa.RemoveAt(index);
+            return Ok("Mahasiswa berhasil dihapus");
+        }
+    }
+}
---
+++ 09_API_Design_dan_Construction_Using_Swagger/TP/Tpmodul9_2311104049/Models/Mahasiswa.cs
@@ -0,0 +1,8 @@
+﻿namespace Tpmodul9_2311104049.Models
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string NIM { get; set; }
+    }
+}
