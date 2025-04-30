523c6a9d6e4ee47ad8638a6cb5f8b48ca63146a8
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-04-30 16:15:00 +0700

    menambahkan modul10

---
+++ 10_Library_Construction/Modul/Modul10/ShowCase/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UtilityLibraries; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int rowCount = 0; 
+
+        while (true)
+        {
+           
+            if (rowCount == 0 || rowCount >= 25)
+                ResetConsole();
+
+            Console.Write("Input your string: ");
+            string input = Console.ReadLine();
+
+            
+            if (string.IsNullOrEmpty(input))
+                break;
+
+            bool isUppercase = input.StartsWithUpper(); 
+            Console.WriteLine($"Input: {input,-30} | Begins with uppercase? : {(isUppercase ? "Yes" : "No")}\n");
+
+            rowCount += 3; 
+        }
+
+
+        void ResetConsole()
+        {
+            if (rowCount > 0)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+
+            Console.Clear();
+            Console.WriteLine("=== Cek Huruf Kapital ===");
+            Console.WriteLine("Tekan <Enter> langsung untuk keluar.\n");
+            rowCount = 3;
+        }
+    }
+}
---
+++ 10_Library_Construction/Modul/Modul10/StringLiblary/StringLiblary.cs
@@ -0,0 +1,13 @@
+﻿namespace UtilityLibraries
+{
+    public static class StringLibrary
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return false;
+
+            return char.IsUpper(str[0]);
+        }
+    }
+}
