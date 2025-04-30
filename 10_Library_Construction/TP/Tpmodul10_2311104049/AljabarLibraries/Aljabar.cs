bed4d4f6329eb688e05f70f26632be0e54982495
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-04-30 17:30:24 +0700

    mengimplementasikan liblary

---
+++ 10_Library_Construction/TP/Tpmodul10_2311104049/AljabarLibraries/Aljabar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+            double diskriminan = b * b - 4 * a * c;
+
+            if (diskriminan < 0)
+                throw new ArgumentException("Persamaan tidak memiliki akar real.");
+
+            double akar1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+            double akar2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+            return new double[] { akar1, akar2 };
+        }
+
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double a2 = a * a;
+            double ab2 = 2 * a * b;
+            double b2 = b * b;
+
+            return new double[] { a2, -ab2, b2 };
+        }
+    }
+}
---
+++ 10_Library_Construction/TP/Tpmodul10_2311104049/MainApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace MainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+            Console.WriteLine($"Akar-akarnya adalah: {akar[0]} dan {akar[1]}");
+
+            var kuadrat = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+            Console.WriteLine($"Hasil kuadrat adalah: {kuadrat[0]}x^2 + {kuadrat[1]}x + {kuadrat[2]}");
+            Console.ReadLine();
+        }
+    }
+}
