92ee975eb08ae08a94c242348077b570a18de491
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-05-09 10:11:05 +0700

    menambahkan syntax modul 12

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/modul12/UnitTest/Test1.cs
@@ -0,0 +1,34 @@
+﻿namespace UnitTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+        public void TestGrade()
+        {
+            String? result1 = modul12.Program.DetermineGrade(90);
+            Assert.AreEqual("A", result1);
+
+            String? result2 = modul12.Program.DetermineGrade(80);
+            Assert.AreEqual("B", result2);
+
+            String? result3 = modul12.Program.DetermineGrade(70);
+            Assert.AreEqual("C", result3);
+
+            String? result4 = modul12.Program.DetermineGrade(60);
+            Assert.AreEqual("D", result4);
+
+            String? result5 = modul12.Program.DetermineGrade(50);
+            Assert.AreEqual("E", result5);
+
+            String? result6 = modul12.Program.DetermineGrade(101);
+            Assert.IsNull(result6);
+
+            String? result7 = modul12.Program.DetermineGrade(-1);
+            Assert.IsNull(result7);
+        }
+    }
+}
---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/modul12/modul12/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace modul12
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string result = DetermineGrade(90);
+            Console.WriteLine(result);
+        }
+
+        public static String? DetermineGrade(int score)
+        {
+
+            if(score > 100 || score < 0 ) return null;
+
+            Dictionary<int, String> GradeMap = new Dictionary<int, String>()
+            {
+                { 90, "A" },
+                { 80, "B" },
+                { 70, "C" },
+                { 60, "D" },
+                { 0, "E" }
+            };
+            foreach (var kv in GradeMap.OrderByDescending(kv => kv.Key))
+            {
+                if (score >= kv.Key)
+                {
+                    return kv.Value;
+                }
+            }
+            return null;
+        }
+    }
+}
