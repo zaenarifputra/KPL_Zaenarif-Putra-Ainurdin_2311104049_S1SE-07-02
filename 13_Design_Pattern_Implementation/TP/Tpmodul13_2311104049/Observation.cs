784b6141dbe712141f8c30d7e545de3030fc1604
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-05-15 10:04:01 +0700

    mengimplementasikan Design Pattern Observer

---
+++ 13_Design_Pattern_Implementation/TP/Tpmodul13_2311104049/Observation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tpmodul13_2311104049
+{
+    public interface IObserver
+    {
+        void Update(string message);
+    }
+    public interface ISubject
+    {
+        void Attach(IObserver observer);
+        void Detach(IObserver observer);
+        void Notify(string message);
+    }
+    public class Subject : ISubject
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+
+        public void Attach(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify(string message)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update(message);
+            }
+        }
+    }
+    public class ConcreteObserver : IObserver
+    {
+        private string _name;
+
+        public ConcreteObserver(string name)
+        {
+            _name = name;
+        }
+
+        public void Update(string message)
+        {
+            Console.WriteLine($"{_name} menerima notifikasi: {message}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 13_Design_Pattern_Implementation/TP/Tpmodul13_2311104049/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Tpmodul13_2311104049;
+
+namespace Tpmodul13_2311104049
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Subject subject = new Subject();
+
+            ConcreteObserver observer1 = new ConcreteObserver("Observer 1");
+            ConcreteObserver observer2 = new ConcreteObserver("Observer 2");
+
+            subject.Attach(observer1);
+            subject.Attach(observer2);
+
+            subject.Notify("Update pertama!");
+
+            subject.Detach(observer1);
+
+            subject.Notify("Update kedua!");
+        }
+    }
+}
